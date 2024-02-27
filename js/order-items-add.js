22a38b28876feced21a139582e21c96eddff6098
Egor Kostiuk <127864304+EgorKostiuk@users.noreply.github.com>
2024-02-27 14:43:17 +0200

    main function Optimize

--- js/order-items-add.js
+++ js/order-items-add.js
@@ -8,49 +8,53 @@ let orderItemsCount = 0;
 function updateTotalAmountDisplay() {
   const orderTotalAmountElement = document.querySelector('.order__total-amount');
   orderTotalAmountElement.textContent = orderTotalAmount;
-}
+};
 // calculates the total Items in the order basket
 function updateTotalOrderItems() {
   const orderTotalCountElement = document.querySelector('.order__count');
   orderTotalCountElement.textContent = orderItemsCount;
-}
+};
 
-productAddBtn.forEach(function (button, index) {
-  button.addEventListener('click', function() {
-    const orderList = document.querySelector('.order__list');
-    const catalogItem = document.querySelector(`.catalog__item:nth-child(${index + 1})`); // Используем :nth-child() для получения нужного элемента
-    const catalogItemInfo = {
-      catalogItemName: catalogItem.querySelector(".product__title button").textContent,
-      catalogItemWeight: catalogItem.querySelector(".product__weight").textContent,
-      catalogItemPrice: +catalogItem.querySelector(".product__price span").textContent, // Преобразуем в число
-      catalogItemImageSrc: catalogItem.querySelector(".product__image").getAttribute("src"),
-    }
-
-    // creates a new item at the end of the order basket
-    const orderItem = createOrderItem(catalogItemInfo);
-    orderList.appendChild(orderItem);
-
-    // adding styles to the element and its visibility
-    orderItem.style.display = 'flex';
-    orderItem.classList.add('order__item');
-
-    // call updateTotalOrderItems()
-    orderItemsCount++
-    updateTotalOrderItems();
-
-    // call updateTotalAmountDisplay()
-    orderTotalAmount += catalogItemInfo.catalogItemPrice;
-    updateTotalAmountDisplay();
-
-    // consts for updateOneItemCount()
-    const countPlusBtn = orderItem.querySelector('.count__plus');
-    const countMinusBtn = orderItem.querySelector('.count__minus');
-    const countAmountElement = orderItem.querySelector('.count__amount');
-
-    countPlusBtn.addEventListener('click', updateOneItemCount.bind(countAmountElement, 1));
-    countMinusBtn.addEventListener('click', updateOneItemCount.bind(countAmountElement, -1));
-  })
+// When you click on the button on the catalog item card, all the functionality of the order basket works
+productAddBtn.forEach((button, index) => {
+  button.addEventListener('click', () => handleProductAddClick(index));
 });
+
+// main function
+function handleProductAddClick(index) {
+  const orderList = document.querySelector('.order__list');
+  const catalogItem = document.querySelector(`.catalog__item:nth-child(${index + 1})`); // Используем :nth-child() для получения нужного элемента
+  const catalogItemInfo = {
+    catalogItemName: catalogItem.querySelector(".product__title button").textContent,
+    catalogItemWeight: catalogItem.querySelector(".product__weight").textContent,
+    catalogItemPrice: +catalogItem.querySelector(".product__price span").textContent, // Преобразуем в число
+    catalogItemImageSrc: catalogItem.querySelector(".product__image").getAttribute("src"),
+  };
+
+  // creates a new item at the end of the order basket
+  const orderItem = createOrderItem(catalogItemInfo);
+  orderList.appendChild(orderItem);
+
+  // adding styles to the element and its visibility
+  orderItem.style.display = 'flex';
+  orderItem.classList.add('order__item');
+
+  // call updateTotalOrderItems()
+  orderItemsCount++
+  updateTotalOrderItems();
+
+  // call updateTotalAmountDisplay()
+  orderTotalAmount += catalogItemInfo.catalogItemPrice;
+  updateTotalAmountDisplay();
+
+  // consts for updateOneItemCount()
+  const countPlusBtn = orderItem.querySelector('.count__plus');
+  const countMinusBtn = orderItem.querySelector('.count__minus');
+  const countAmountElement = orderItem.querySelector('.count__amount');
+
+  countPlusBtn.addEventListener('click', updateOneItemCount.bind(countAmountElement, 1));
+  countMinusBtn.addEventListener('click', updateOneItemCount.bind(countAmountElement, -1));
+};
 
 // counts the quantity of each item in the order basket and deletes if there are 0 items
 function updateOneItemCount(delta) {
@@ -73,6 +77,5 @@ function updateOneItemCount(delta) {
     orderTotalAmount += delta * parseFloat(productPriceElement.textContent);
     updateTotalAmountDisplay();
     countAmountElement.textContent = currentAmount;
-  }
-}
-
+  };
+};
