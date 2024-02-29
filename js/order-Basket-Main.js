33bc1ff122bf09c7975443aed868abfc8702f428
Egor Kostiuk <127864304+EgorKostiuk@users.noreply.github.com>
2024-02-29 14:28:10 +0200

    js file name fix

--- js/order-Basket-Main.js
+++ js/order-Basket-Main.js
@@ -1,4 +1,4 @@
-import { createOrderItem } from './create-Order-Basket-Item.js';
+import { createOrderItem } from './create-order-basket-item,js';
 
 const productAddBtn = document.querySelectorAll('.product__add');
 let orderTotalAmount = 0;
