a99d1c360e1232064b9735cdeccae986a9d2be89
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-17 10:11:05 +0300

    update: implement vite + react

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-export const emptyOrderBasket = document.createElement('li');
-  emptyOrderBasket.classList.add('order__item');
-  emptyOrderBasket.innerHTML =  `<span>Корзина пустая</span>`;
-  emptyOrderBasket.style.display = 'flex';
-  emptyOrderBasket.style.justifyContent = 'center';
-
-
