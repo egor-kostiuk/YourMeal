9db4c1aebd89106b8608406fd2405ca251d0984a
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-18 17:20:45 +0300

    feat: add burgers page

---
+++ src/components/pages/Burgers.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { MainLayout } from '../layouts/MainLayout'
+import { Main } from '../main/Main'
+
+export const Burgers: FC = () => {
+  return (
+    <MainLayout>
+      <Main />
+    </MainLayout>
+  )
+}
